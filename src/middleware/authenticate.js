bfe8a4f31b47c3555d86883ca480e3ffd5738f41
SHG-345454257 <hodapochta0usba936@duck.com>
2026-03-30 12:45:34 +0700

    Update authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,11 +1,10 @@
+//проверка JWT токена и добавляеи данные пользователя в запрос
 import jwt from "jsonwebtoken";
 import config from "../config.js";
 import AppError from "../utils/appError.js";
-
 export default function authenticate(req, res, next) {
   const token = req.cookies.accessToken;
   if (!token) return next(new AppError("Вы не авторизованы", 401));
-
   try {
     const decoded = jwt.verify(token, config.jwt.secret);
     req.user = decoded;
